c94d5dabc5fabb5d4fe1fc4cd9de57727f77a977
xyyx <xyyx@mail.ru>
2022-11-26 20:07:32 +0300

    Add libinit to SafetyNet workaround without Magisk

--- BoardConfig-evolution.mk
+++ BoardConfig-evolution.mk
@@ -1,3 +1,9 @@
 BUILD_BROKEN_DUP_RULES := true
 
+DEVICE_PATH := device/google/bluejay
+
 include vendor/google/bluejay/BoardConfigVendor.mk
+
+# Init
+TARGET_INIT_VENDOR_LIB := //$(DEVICE_PATH):libinit_bluejay
+TARGET_RECOVERY_DEVICE_MODULES := libinit_bluejay
